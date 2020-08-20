d02e10d2c1a4cf4268419a6ed5c23d6bfbdf9791
nzeal <nshukla@yahoo.com>
2020-08-20 16:31:24 +0200

    adding the code _example_python

---
+++ shopping/test.py
@@ -0,0 +1,7 @@
+from utility import multiply, divide
+from shopping.more_shopping.shopping_cart import buy 
+
+if __name__ == '__main__':
+	print(buy('apple'))
+	print(multiply(5,2))
+	print(divide(5,2))
